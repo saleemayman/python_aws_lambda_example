31465a2e0c26df0e51963f6aa6e3aa76429665b1
AS <7293160+saleemayman@users.noreply.github.com>
2024-01-14 21:15:02 +0500

    simplified lambda_handler input.

--- python/src/battery_status.py
+++ python/src/battery_status.py
@@ -11,9 +11,7 @@ from definitions import BatteryHealth, InputEvent, log
 
 def lambda_handler(event: Dict[str, Any], context: LambdaContext) -> Dict[str, Any]:
     try:
-        request_body = json.loads(event["body"])
-
-        result = parse_battery_health(InputEvent(**request_body))
+        result = parse_battery_health(InputEvent(**event))
         logging.info(
             log("Finished parsing battery health", result=result.model_dump_json())
         )
@@ -23,15 +21,15 @@ def lambda_handler(event: Dict[str, Any], context: LambdaContext) -> Dict[str, A
             "body": json.dumps({"result": result.model_dump_json()}),
         }
     except ValidationError as e:
-        logging.error(log("Error parsing and processing input", error=e))
+        logging.error(log("Error parsing and processing input", error=str(e)))
         response = {
             "statusCode": 400,
-            "body": json.dumps({"error": "Error parsing input", "details": e}),
+            "body": json.dumps({"error": "Error parsing input", "details": str(e)}),
         }
     except Exception as e:
         response = {
             "statusCode": 500,
-            "body": json.dumps({"error": "Unexpected server error", "details": e}),
+            "body": json.dumps({"error": "Unexpected server error", "details": str(e)}),
         }
 
     return response
@@ -51,9 +49,9 @@ def parse_battery_health(event: InputEvent) -> BatteryHealth:
 
 # if __name__ == "__main__":
 #     test_inputs = [
-#         {"body": '{"device": "device_1", "payload": "F1E6E63676C75000"}'},
-#         {"body": '{"device": "device_2", "payload": "9164293726C85400"}'},
-#         {"body": '{"device": "device_3", "payload": "6188293726C75C00"}'},
+#         {"device": "device_1", "payload": "F1E6E63676C75000"},
+#         {"device": "device_2", "payload": "9164293726C85400"},
+#         {"device": "device_3", "payload": "6188293726C75C00"},
 #     ]
 
 #     for test in test_inputs:
